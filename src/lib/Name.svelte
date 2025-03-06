30aef05f8104e8abb583ba87c539ccb39e4a182d
Saman <saman.ashoori@outlook.com>
2025-03-06 21:29:34 +0000

    added a reverse name in name.svelte using legacy mode

--- src/lib/Name.svelte
+++ src/lib/Name.svelte
@@ -1,16 +1,19 @@
 <script lang=ts>
-    let userName = $state('');
-    let userNameBackwards;
+    let userName = '';
+    $:reversedName = userName.split('').reverse().join('')
     
     
 
 </script>
 
 <input bind:value={userName} placeholder="Enter your name" />
-<p>Hello <span class='name'>{userName || 'pal'}!</span></p>
+<p>Hello <span class='name'>{userName || 'pal'}!</span> your name backwards is <span class='nameback'>{reversedName || 'lap'}</span></p>
 
 <style>
 .name {
     color: red;
+}
+.nameback {
+    color: blue;
 }
 </style>
